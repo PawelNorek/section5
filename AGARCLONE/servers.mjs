f2d969f4dd7d802ae3fe866849f617c90c0a25ca
pawel <pawel@4nol74.cloud>
2023-09-14 21:06:32 +0200

    Initial server/file structure

---
+++ AGARCLONE/servers.mjs
@@ -0,0 +1,23 @@
+//Agar.io clone
+
+import express from 'express'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+
+const app = express()
+const httpServer = createServer(app)
+const io = new Server(httpServer)
+
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.use(express.static(__dirname + '/public'))
+
+io.on('connection', socket => {
+	socket.on('event', () => {})
+})
+
+httpServer.listen(3001)
