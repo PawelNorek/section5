f935ce09cde39497b2239b062da7eb42706104b1
pawel <pawel@4nol74.cloud>
2023-09-19 10:06:57 +0200

    Refactored - tests not working yet

--- public/socketStuff.js
+++ public/socketStuff.js
@@ -1,6 +1,13 @@
 const socket = io.connect('http://192.168.1.191:3001')
 
-socket.on('init', initData => {
+const init = async () => {
+	const initOrbs = await socket.emitWithAck('init', {
+		playerName: player.name,
+	})
+	orbs = initOrbs
+	draw()
+}
+
+socket.on('initReturn', initData => {
 	// console.log(initData)
-	orbs = initData.orbs
 })
--- socketStuff/socketMain.mjs
+++ socketStuff/socketMain.mjs
@@ -2,19 +2,41 @@
 
 import { io, app } from '../servers.mjs'
 
+import Player from './classes/Player.mjs'
+import PlayerConfig from './classes/PlayerConfig.mjs'
+import PlayerData from './classes/PlayerData.mjs'
+
 import Orb from './classes/Orb.mjs'
 
 const orbs = []
+const settings = {
+	defaultNumberOfOrbs: 500,
+	defaultSpeed: 6,
+	defaultSize: 6,
+	defaultZoom: 1.5,
+	worldWidth: 500,
+	worldHeight: 500,
+	defaultGenericOrbSize: 5,
+}
+
+const players = []
 
 initGame()
 // console.log(orbs)
 
 io.on('connect', socket => {
-	socket.emit('init', { orbs })
+	socket.on('init', (playerObj, ackCallback) => {
+		const playerName = playerObj.playerName
+		const playerConfig = new PlayerConfig(settings)
+		const playerData = new PlayerData(playerName, settings)
+		const player = new Player(socket.id, playerConfig, playerData)
+		players.push(player)
+		ackCallback(orbs)
+	})
 })
 
 function initGame() {
-	for (let i = 0; i < 500; i++) {
-		orbs.push(new Orb())
+	for (let i = 0; i < settings.defaultNumberOfOrbs; i++) {
+		orbs.push(new Orb(settings))
 	}
 }
